82bcc17f42b561881626542fb907a59f1257699a
VanoKi <kirsvano@gmail.com>
2024-10-26 22:49:03 +0300

    feat: solve findAllOccurrences

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -385,8 +385,9 @@ function getFalsyValuesCount(arr) {
  *    [ null, undefined, null ], null => 2
  *    [ true, 0, 1, 'true' ], true => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  // throw new Error('Not implemented');
+  return arr.filter((i) => i === item).length;
 }
 
 /**
