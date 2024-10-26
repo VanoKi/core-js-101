6be635b6ac26e1d3915ba6b479f8182e15ec3b38
VanoKi <kirsvano@gmail.com>
2024-10-26 21:29:22 +0300

    feat: solve double array

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -53,8 +53,9 @@ function generateOdds(/* len */) {
  *    [0, 1, 2, 3, 4, 5] => [0, 1, 2, 3, 4, 5,   0, 1, 2, 3, 4, 5]
  *    [] => []
  */
-function doubleArray(/* arr */) {
-  throw new Error('Not implemented');
+function doubleArray(arr) {
+  // throw new Error('Not implemented');
+  return arr.concat(arr);
 }
 
 
